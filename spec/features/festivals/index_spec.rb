b6a33fb926f42fde9ef652d6cbd3766d46b88229
Reilly Miller <reillyjmiller31@gmail.com>
2023-03-24 08:57:40 -0600

    user story 6

--- spec/features/festivals/index_spec.rb
+++ spec/features/festivals/index_spec.rb
@@ -44,7 +44,7 @@ RSpec.describe "/festivals", type: :feature do
     it 'I see the Festival with that id, including its attributes' do
 
       visit "/festivals/#{summer_camp.id}"
-      save_and_open_page
+      # save_and_open_page
 
       expect(page).to have_content("#{summer_camp.name}")
       expect(page).to have_content("City/State: #{summer_camp.city}")
@@ -52,6 +52,45 @@ RSpec.describe "/festivals", type: :feature do
       expect(page).to have_content("Ticket Price: #{summer_camp.ticket_price}")
       expect(page).to have_content("Dates: #{summer_camp.dates}")
       expect(page).to have_content("RV Hookup? #{summer_camp.rv_hookup}")
+    end
+  end
+  #User Story 6
+  describe "As a visitor, when I visit the Festival index" do
+    
+    let!(:summer_camp) { Festival.create!(name: "Summer Camp Music Festival",
+                                          city: "Chillicothe, IL",
+                                          kid_friendly: true,
+                                          ticket_price: 600,
+                                          dates: "May 26th - 28th, 2023",
+                                          rv_hookup: false,
+                                          created_at: 1.day.ago)}
+    let!(:all_good) { Festival.create!(name: "All Good Music Festival",
+                                       city: "Masontown, WV",
+                                       kid_friendly: true,
+                                       ticket_price: 550,
+                                       dates: "July 15th - 17th, 2023",
+                                       rv_hookup: false,
+                                       created_at: 3.days.ago)}     
+    let!(:arise) { Festival.create!(name: "Arise Music Festival",
+                                    city: "Loveland, CO",
+                                    kid_friendly: true,
+                                    ticket_price: 400,
+                                    dates: "August 2nd - 4th, 2023",
+                                    rv_hookup: true,
+                                    created_at: 2.days.ago)}
+    it 'I see that records are ordered by most recently created first' do
+      festivals = Festival.all
+      visit "/festivals"
+      # save_and_open_page
+      assert festivals.sort_by_most_recent
+      # require 'pry'; binding.pry
+    end
+    
+    it 'Next to each of the records, I see when it was created' do
+      visit "/festivals"
+      
+      save_and_open_page
+      expect(page).to have_content("#{summer_camp.created_at}")
     end
   end
 end
