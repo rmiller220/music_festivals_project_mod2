c15739ec90e7fef334ddd3db77a5ee3ebb38ce43
Reilly Miller <reillyjmiller31@gmail.com>
2023-03-25 18:47:52 -0600

    user story 9

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,41 +5,41 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-summer_camp = Festival.create!(name: "Summer Camp Music Festival",
+@summer_camp = Festival.create!(name: "Summer Camp Music Festival",
                                city: "Chillicothe, IL",
                                kid_friendly: true,
                                ticket_price: 600,
                                dates: "May 26th - 28th, 2023",
                                rv_hookup: false,
                                created_at: 1.day.ago)
-all_good = Festival.create!(name: "All Good Music Festival",
+@all_good = Festival.create!(name: "All Good Music Festival",
                             city: "Masontown, WV",
                             kid_friendly: true,
                             ticket_price: 550,
                             dates: "July 15th - 17th, 2023",
                             rv_hookup: false,
                             created_at: 3.days.ago)     
-sts9 = Artist.create!(name: "STS9",
+@sts9 = Artist.create!(name: "STS9",
                       explicit_content: false,
                       performance_day: "Friday, Saturday",
                       number_of_performances: 2,
                       festival_appearances: 10,
-                      festival: summer_camp)
-les = Artist.create!(name: "Les Claypool",
+                      festival: @summer_camp)
+@les = Artist.create!(name: "Les Claypool",
                      explicit_content: false,
                      performance_day: "Saturday",
                      number_of_performances: 1,
                      festival_appearances: 5,
-                     festival: all_good)
-bone_thugs = Artist.create!(name: "Bone Thugs in Harmony",
+                     festival: @all_good)
+@bone_thugs = Artist.create!(name: "Bone Thugs in Harmony",
                             explicit_content: true,
                             performance_day: "Sunday",
                             number_of_performances: 1,
                             festival_appearances: 3,
-                            festival: summer_camp)
-bassnectar = Artist.create!(name: "Bassnectar",
+                            festival: @summer_camp)
+@bassnectar = Artist.create!(name: "Bassnectar",
                             explicit_content: false,
                             performance_day: "Friday",
                             number_of_performances: 1,
                             festival_appearances: 2,
-                            festival: all_good)
+                            festival: @all_good)
--- spec/features/festivals/index_spec.rb
+++ spec/features/festivals/index_spec.rb
@@ -218,7 +218,81 @@ RSpec.describe "/festivals", type: :feature do
       
       expect(current_path).to eq("/artists")
 
+      visit "/festivals/#{summer_camp.id}/artists"
+      
       click_on artists
+
+      expect(current_path).to eq("/artists")
+    end
+  end
+
+  describe "As a visitor, when I visit any page on the site" do
+    let!(:summer_camp) { Festival.create!(name: "Summer Camp Music Festival",
+                                          city: "Chillicothe, IL",
+                                          kid_friendly: true,
+                                          ticket_price: 600,
+                                          dates: "May 26th - 28th, 2023",
+                                          rv_hookup: false,
+                                          created_at: 1.day.ago)}
+    let!(:all_good) { Festival.create!(name: "All Good Music Festival",
+                                       city: "Masontown, WV",
+                                       kid_friendly: true,
+                                       ticket_price: 550,
+                                       dates: "July 15th - 17th, 2023",
+                                       rv_hookup: false,
+                                       created_at: 3.days.ago)}     
+    let!(:sts9) { Artist.create!(name: "STS9",
+                                 explicit_content: false,
+                                 performance_day: "Friday, Saturday",
+                                 number_of_performances: 2,
+                                 festival_appearances: 10,
+                                 festival: summer_camp)}
+    let!(:sts9_a) { Artist.create!(name: "STS9",
+                                 explicit_content: false,
+                                 performance_day: "Saturday",
+                                 number_of_performances: 1,
+                                 festival_appearances: 5,
+                                 festival: all_good)}
+    let!(:bone_thugs) { Artist.create!(name: "Bone Thugs in Harmony",
+                                       explicit_content: true,
+                                       performance_day: "Sunday",
+                                       number_of_performances: 1,
+                                       festival_appearances: 3,
+                                       festival: summer_camp)}
+    let!(:bassnectar) { Artist.create!(name: "Bassnectar",
+                                       explicit_content: false,
+                                       performance_day: "Friday",
+                                       number_of_performances: 1,
+                                       festival_appearances: 2,
+                                       festival: all_good)}
+    it "Then I see a link at the top of the page that takes me to the Festival Index" do
+      festivals = "Festivals"
+
+      visit "/festivals"
+
+      click_on festivals
+
+      expect(current_path).to eq("/festivals")
+
+      visit "/festivals/#{summer_camp.id}"
+
+      click_on festivals
+      
+      expect(current_path).to eq("/festivals")
+      
+      visit "/artists"
+
+      click_on festivals
+      
+      expect(current_path).to eq("/festivals")
+
+      visit "/artists/#{sts9.id}"
+
+      click_on festivals
+      
+      expect(current_path).to eq("/festivals")
+
+      click_on festivals
 
       visit "/festivals/#{summer_camp.id}/artists"
     end
